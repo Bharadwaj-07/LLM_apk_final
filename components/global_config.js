6e792c89f03ee5a76833788c039f345c093ab464
Bharadwaj-07 <gbjbharath@gmail.com>
2025-02-16 23:08:57 +0530

    deleted port

--- components/global_config.js
+++ components/global_config.js
@@ -2,7 +2,7 @@
 // Load environment variables from .env file
 
 export const GLOBAL_CONFIG = {
-  SYSTEM_IP: process.env.SYSTEM_IP || "10.25.67.62", // Default IP if not set
+  SYSTEM_IP:"ise-project-t7w6.onrender.com", // Default IP if not set
   PORT: process.env.PORT || 5000 // Default port 5000 if not set
 };
 
